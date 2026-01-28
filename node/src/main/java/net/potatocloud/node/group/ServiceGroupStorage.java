5b6d2d90d09c0a0c49c125095732603bf2f01855
jirmjahu <159639233+jirmjahu@users.noreply.github.com>
2026-01-28 13:13:25 +0100

    Update group file layout

--- node/src/main/java/net/potatocloud/node/group/ServiceGroupStorage.java
+++ node/src/main/java/net/potatocloud/node/group/ServiceGroupStorage.java
@@ -17,22 +17,27 @@ public class ServiceGroupStorage {
     @SneakyThrows
     public void saveToFile(ServiceGroup group, Path directory) {
         final YamlFile config = new YamlFile(directory.resolve(group.getName() + ".yml").toFile());
+
         config.set("name", group.getName());
-        config.set("platform", group.getPlatform().getName() + "-" + group.getPlatformVersionName());
-        config.set("templates", group.getServiceTemplates());
-        config.set("minOnlineCount", group.getMinOnlineCount());
-        config.set("maxOnlineCount", group.getMaxOnlineCount());
-        config.set("maxPlayers", group.getMaxPlayers());
-        config.set("maxMemory", group.getMaxMemory());
-        config.set("fallback", group.isFallback());
-        config.set("static", group.isStatic());
-        config.set("startPriority", group.getStartPriority());
-        config.set("startPercentage", group.getStartPercentage());
-        config.set("javaCommand", group.getJavaCommand());
+        config.set("platform", group.getPlatformName());
+        config.set("platform-version", group.getPlatformVersionName());
+        config.set("java-command", group.getJavaCommand());
 
         if (!group.getCustomJvmFlags().isEmpty()) {
-            config.set("customJvmFlags", group.getCustomJvmFlags());
+            config.set("jvm-flags", group.getCustomJvmFlags());
         }
+
+        config.set("max-players", group.getMaxPlayers());
+        config.set("max-memory", group.getMaxMemory());
+        config.set("min-online-count", group.getMinOnlineCount());
+        config.set("max-online-count", group.getMaxOnlineCount());
+
+        config.set("static", group.isStatic());
+        config.set("fallback", group.isFallback());
+        config.set("start-priority", group.getStartPriority());
+        config.set("start-percentage", group.getStartPercentage());
+
+        config.set("templates", group.getServiceTemplates());
 
         if (!group.getProperties().isEmpty()) {
             for (Property<?> property : group.getProperties()) {
@@ -62,26 +67,21 @@ public class ServiceGroupStorage {
             }
         }
 
-        final String platformFullName = config.getString("platform");
-        final String[] parts = platformFullName.split("-", 2);
-        final String platformName = parts[0];
-        final String platformVersion = parts[1];
-
         return new ServiceGroupImpl(
                 config.getString("name"),
-                platformName,
-                platformVersion,
+                config.getString("platform"),
+                config.getString("platform-version"),
                 config.getStringList("templates"),
-                config.getInt("minOnlineCount"),
-                config.getInt("maxOnlineCount"),
-                config.getInt("maxPlayers"),
-                config.getInt("maxMemory"),
+                config.getInt("min-online-count"),
+                config.getInt("max-online-count"),
+                config.getInt("max-players"),
+                config.getInt("max-memory"),
                 config.getBoolean("fallback"),
                 config.getBoolean("static"),
-                config.getInt("startPriority"),
-                config.getInt("startPercentage"),
-                config.getString("javaCommand"),
-                config.getStringList("customJvmFlags"),
+                config.getInt("start-priority"),
+                config.getInt("start-percentage"),
+                config.getString("java-command"),
+                config.getStringList("jvm-flags"),
                 properties
         );
     }
