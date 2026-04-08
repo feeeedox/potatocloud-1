5f1added8e1cadaab21784f1adcbd44adf9348b0
jirmjahu <159639233+jirmjahu@users.noreply.github.com>
2026-04-08 17:23:39 +0200

    Small code changes

--- api/src/main/java/net/potatocloud/api/CloudAPI.java
+++ api/src/main/java/net/potatocloud/api/CloudAPI.java
@@ -10,7 +10,6 @@ import net.potatocloud.api.service.Service;
 import net.potatocloud.api.service.ServiceManager;
 import net.potatocloud.api.utils.version.Version;
 
-@Getter
 public abstract class CloudAPI {
 
     /**
