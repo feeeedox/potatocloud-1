df752c508b13118825cadfdf806af30bd26207c1
jirmjahu <159639233+jirmjahu@users.noreply.github.com>
2026-06-01 17:52:43 +0200

    Refactor NodeJoinListener

--- node/src/main/java/net/potatocloud/node/cluster/listeners/NodeJoinListener.java
+++ node/src/main/java/net/potatocloud/node/cluster/listeners/NodeJoinListener.java
@@ -3,6 +3,7 @@ package net.potatocloud.node.cluster.listeners;
 import net.potatocloud.api.cluster.ClusterNode;
 import net.potatocloud.api.logging.Logger;
 import net.potatocloud.network.ConnectionType;
+import net.potatocloud.network.NetworkConnection;
 import net.potatocloud.network.packet.PacketContext;
 import net.potatocloud.network.packet.PacketListener;
 import net.potatocloud.network.packet.packets.cluster.NodeDiscoveryPacket;
@@ -27,24 +28,25 @@ public class NodeJoinListener implements PacketListener<NodeJoinPacket> {
     @Override
     public void handle(PacketContext<NodeJoinPacket> ctx) {
         final NodeJoinPacket packet = ctx.packet();
+        final NetworkConnection connection = ctx.connection();
         final UUID nodeId = packet.nodeId();
 
         if (nodeId.equals(localNode.id())) {
             return;
         }
 
-        ctx.connection().type(ConnectionType.NODE);
+        connection.type(ConnectionType.NODE);
 
-        final ClusterNodeImpl node = new ClusterNodeImpl(nodeId, packet.name(), packet.host(), packet.port(), packet.startedAt(), ctx.connection());
+        final ClusterNodeImpl node = new ClusterNodeImpl(nodeId, packet.name(), packet.host(), packet.port(), packet.startedAt(), connection);
         clusterManager.add(node);
 
-        if (clusterManager.isOutbound(ctx.connection())) {
+        if (clusterManager.isOutbound(connection)) {
             logger.info("Connected to cluster node &a" + node.name() + " &8(&a" + node.host() + "&8:&a" + node.port() + "&8)");
         } else {
             logger.info("Cluster node &a" + node.name() + " &7connected to the cluster &8(&a" + node.host() + "&8:&a" + node.port() + "&8)");
-            ctx.connection().send(new NodeJoinPacket(localNode.id(), localNode.name(), localNode.host(), localNode.port(), localNode.startedAt()));
+            connection.send(new NodeJoinPacket(localNode.id(), localNode.name(), localNode.host(), localNode.port(), localNode.startedAt()));
 
-            ctx.connection().send(new NodeDiscoveryPacket(
+            connection.send(new NodeDiscoveryPacket(
                     clusterManager.remoteNodes().stream()
                             .filter(n -> !n.id().equals(nodeId))
                             .map(n -> (ClusterNode) n)
