9c7448812536106f04aeef47cd9ee6e77ee62867
Florian <67076970+feeeedox@users.noreply.github.com>
2026-06-07 17:42:01 +0200

    Add log listener functionality to Screen class

--- node/src/main/java/net/potatocloud/node/screen/Screen.java
+++ node/src/main/java/net/potatocloud/node/screen/Screen.java
@@ -2,7 +2,10 @@ package net.potatocloud.node.screen;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.function.Consumer;
 
 public class Screen {
 
@@ -10,10 +13,12 @@ public class Screen {
 
     private final String name;
     private final List<String> cachedLogs;
+    private final Set<Consumer<String>> logListeners;
 
     public Screen(String name) {
         this.name = name;
         this.cachedLogs = new CopyOnWriteArrayList<>();
+        this.logListeners = new CopyOnWriteArraySet<>();
     }
 
     public String name() {
@@ -27,9 +32,24 @@ public class Screen {
     public void addLog(String log) {
         synchronized (cachedLogs) {
             if (cachedLogs.size() >= 1000) {
-                cachedLogs.remove(0);
+                cachedLogs.clear();
             }
             cachedLogs.add(log);
         }
+        for (Consumer<String> listener : logListeners) {
+            try {
+                listener.accept(log);
+            } catch (Exception ignored) {
+            }
+        }
     }
+    
+    public void subscribe(Consumer<String> listener) {
+        logListeners.add(listener);
+    }
+
+    public void unsubscribe(Consumer<String> listener) {
+        logListeners.remove(listener);
+    }
+
 }
