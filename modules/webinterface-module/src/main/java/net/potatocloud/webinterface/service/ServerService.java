adfe582bcae07c33c74a7530028167e7add1afea
Florian Ohldag <mrminecrafttv19@gmail.com>
2026-06-08 20:08:24 +0200

    feat(ServerService): add method to stop services

--- modules/webinterface-module/src/main/java/net/potatocloud/webinterface/service/ServerService.java
+++ modules/webinterface-module/src/main/java/net/potatocloud/webinterface/service/ServerService.java
@@ -2,6 +2,7 @@ package net.potatocloud.webinterface.service;
 
 import lombok.RequiredArgsConstructor;
 import net.potatocloud.api.CloudAPI;
+import net.potatocloud.webinterface.WebInterfaceModule;
 import net.potatocloud.webinterface.dto.service.ServerOverviewDto;
 
 import java.util.List;
@@ -15,6 +16,13 @@ public class ServerService {
         return cloudAPI.getServiceManager().getAllServices().stream()
                 .map(ServerOverviewDto::from)
                 .toList();
+    }
+
+    public void stopService(String serviceId) {
+        cloudAPI.getServiceManager().stopService(serviceId);
+
+        WebInterfaceModule.getInstance().
+
     }
 
     public ServerOverviewDto getService(String name) {
