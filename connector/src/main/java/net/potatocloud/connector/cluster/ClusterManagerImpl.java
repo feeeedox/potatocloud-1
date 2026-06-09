43034fdf912de07be31c15b81c1c77037db6e1ef
jirmjahu <159639233+jirmjahu@users.noreply.github.com>
2026-06-09 18:21:54 +0200

    Fix

--- connector/src/main/java/net/potatocloud/connector/cluster/ClusterManagerImpl.java
+++ connector/src/main/java/net/potatocloud/connector/cluster/ClusterManagerImpl.java
@@ -35,14 +35,14 @@ public class ClusterManagerImpl implements ClusterManager {
     }
 
     @Override
-    public Collection<ClusterNode> nodes() {
+    public List<ClusterNode> nodes() {
         final List<ClusterNode> all = new ArrayList<>(nodes.values());
         all.add(localNode);
         return Collections.unmodifiableList(all);
     }
 
     @Override
-    public Optional<ClusterNode> get(String name) {
+    public Optional<ClusterNode> find(String name) {
         if (localNode != null && localNode.name().equals(name)) {
             return Optional.of(localNode);
         }
